84b894d5c0b300d387c29d0e1eb3e4fe42458bee
Changmin Kim <ksjchm4@gmail.com>
2024-11-29 13:15:23 +0900

    set. 초기 세팅

---
+++ src/main/java/com/utaemin/utaemin/UTaeMinApplication.java
@@ -0,0 +1,13 @@
+package com.utaemin.utaemin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UTaeMinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UTaeMinApplication.class, args);
+    }
+
+}
